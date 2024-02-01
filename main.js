3deb1fcfe12d6531b1252c3f8f95079f398384e0
madhavimani <152162952+madhavimani@users.noreply.github.com>
2024-02-01 16:49:28 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstname="NIKHIL";
+ console.log(firstname);
+
+ var lastname="LAKKARAJU";
+ console.log(lastname);
+ 
+ var lastname=firstname.concat(lastname);
+            console.log(lastname);
+
+ var a = 10;
+ var b = 5;           
